556dba9ffce294ce2c61803c6046f5c4d0a87272
frojoe12 <45601644+frojoe12@users.noreply.github.com>
2020-11-02 15:24:05 +0000

    Update change_thumbnail_product_archive.php

--- change_thumbnail_product_archive.php
+++ change_thumbnail_product_archive.php
@@ -1,6 +1,11 @@
 <?php
-
-// add to functions.php file in your plugin/theme/child theme
+/*
+    Description: alter the thumbnail image displayed on the product archive page
+    -- woocommerce default is set to square shape, so handy if you need to maintain the original's aspect ratio
+    
+    Add below to functions.php file in your custom plugin/theme/child theme directory
+    NB: CUSTOMPLUGINNAME should be changed to your own unique plugin/theme template name to prevent conflicts with other plugins or themes
+*/    
 
 function CUSTOMPLUGINNAME_customise_product_thumbnail_for_product_list() {
     the_post_thumbnail();
